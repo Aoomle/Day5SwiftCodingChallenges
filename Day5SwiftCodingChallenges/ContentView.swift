58a1d7e428ed4f8a558cd215ec1bbb012b6baf80
Abdulmalik Muhammad <bawasuru@gmail.com>
2021-02-02 22:12:37 +0100

    Initial Commit

---
+++ Day5SwiftCodingChallenges/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day5SwiftCodingChallenges
+//
+//  Created by Abdulmalik Muhammad on 02/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
